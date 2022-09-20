31fda08a19093c4a4dd3ac23e29e24a954510eaa
Raphael Lüthy <raphael@famluethy.chraphael@famluethy.chraphael@famluethy.ch>
2022-09-20 17:21:42 +0200

    added more lines

--- week1/functionsTest/functionsTest.js
+++ week1/functionsTest/functionsTest.js
@@ -5,6 +5,20 @@ content.push(1 === 1);
 const fun1 = () => 1;
 const fun2 = (args) => args;
 const fun3 = (args) => args === undefined;
+function doit(whatToDo) {
+	return function bla(arg) {
+		return whatToDo(arg);
+	};
+}
+
+const doit2 = (callback) => (args) => callback(args);
+const doFun2 = doit2(fun2);
+
+content.push(doFun2(10));
+
+content.push(doit2(fun2)('doit2'));
+
+content.push(doit(fun2)('doit1'));
 
 content.push(fun1() === 1);
 content.push(fun1(42) === 42);
